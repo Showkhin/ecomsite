651d66a188e8a1ca132eb357ed9bde1ada284d9b
Tousif Mahmud Showkhin <tm.showkhin007@gmail.com>
2022-03-01 14:55:49 +0600

    first commit

---
+++ shop/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Products
+from django.core.paginator import Paginator
+
+
+# Create your views here.
+
+
+def index(request):
+    product_object = Products.objects.all()
+# search code
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_object = product_object.filter(title__icontains=item_name)
+# paginator code
+    paginator = Paginator(product_object, 8)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
+
+    return render(request, 'shop/index.html', {'product_object': product_object})
+
+
+def detail(request, id):
+    product_object1 = Products.objects.get(id=id)
+    return render(request, 'shop/detail.html', {'product_object1': product_object1})
